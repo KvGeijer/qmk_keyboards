80e03512d2de2b9099d3743931c27dbcfe108a29
Kåre von Geijer <kare.kvg@gmail.com>
2024-10-08 09:54:01 +0200

    Modify caps word for swedish layout
    For example switching KC_MINS to SE_MINS.

--- keyboards/crkbd/keymaps/swe-colemak/keymap.c
+++ keyboards/crkbd/keymaps/swe-colemak/keymap.c
@@ -103,3 +103,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
   return true;
 }
+
+/// Modify caps word to suit swedish keyboard
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        // case KC_MINS: // Removed as the SE layout changes this key
+        // Added keycodes for Swedish keyboard:
+        case SE_MINS: // -
+        case SE_ADIA: // Ä
+        case SE_ARNG: // Å
+        case SE_ODIA: // Ö
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        // case KC_UNDS:
+        case SE_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
